bf88fa4a56c7eb34c289769f92b1e85a1d4e13a0
Gadepalli Aditya <gadepalliaditya1998@gmail.com>
2022-10-09 13:47:58 +0530

    added template for login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'split-expense-ui';
+
+  constructor(private routerService: Router) {}
+
+  ngOnInit(): void {
+    if (localStorage.getItem('userToken')) {
+      console.log('TODO: Implement home page');
+    } else {
+      this.routerService.navigateByUrl('login');
+    }
+  }
 }
